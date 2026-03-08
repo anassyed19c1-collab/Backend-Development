3625853606e7abbd4246eb21119631bb8f468698
Syed Muhammad Anas <anas.syed19c1@gmail.com>
2026-03-08 19:39:53 +0500

    changes

---
+++ Lecture_03_Event_Loop/user/user.js
@@ -0,0 +1,69 @@
+import http from 'http';
+import fs from 'fs'
+import { buffer, json } from 'stream/consumers';
+
+const server = http.createServer((req, res) => {
+    // console.log(req);
+
+    console.log(req.url, req.method);
+
+
+    if (req.url === "/") {
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>');
+        res.write('<head><title>Complete Coding</title></head>');
+        res.write('<body><h1>Enter Your Details:</h1>');
+        res.write('<form action="/submit-details" method="POST" >');
+        res.write('<input type="text" name="username" placeholder="Enter your name"><br>');
+        res.write('<label for="male">Male</label>')
+        res.write('<input type="radio" id="male" name="gender" value="male" />')
+        res.write('<label for="female">Female</label>')
+        res.write('<input type="radio" id="female" name="gender" value="female" />')
+        res.write('<br><input type="submit" value="Submit">');
+        res.write('</form>');
+        res.write('</body>');
+        res.write('</html>');
+        return res.end();
+    } else if  ( req.method == "POST" && req.url.toLowerCase() === "/submit-details") {
+
+
+
+        const body = []
+
+        req.on('data', (chunk) => {
+            console.log(chunk);
+            body.push(chunk)
+        });
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            console.log(parsedBody);
+            const params = new URLSearchParams(parsedBody);
+            const bodyObject = Object.fromEntries(params)
+            console.log(bodyObject)
+
+            // In Lecture 03 remove "Sync" method to execute this code asynchronously
+            // When you write only "fs.writeFile", make sure you handle the error, otherwise the server will stopped
+            fs.writeFile('userInfo.txt', JSON.stringify(bodyObject), error => {
+                console.log("File Written Successfully");
+                
+                res.statusCode = 302;
+                res.setHeader('Location', '/')
+                return res.end
+            })
+        })
+    } else {
+        res.setHeader('Content-Type', 'text/html')
+        res.write('<html>')
+        res.write('<head><title>Hello World!</title></head>')
+        res.write('<body><h1>Details Page</h1></body>')
+        res.write('</html>')
+        res.end()
+    }
+        
+
+})
+
+
+server.listen(3000, () => {
+    console.log('Server Started!');
+})
